7c6e00c24b653fb0d7b7151d8173f5f05dbab5b3
YongJae Kim <golddragon939@gmail.com>
2023-10-02 23:45:08 +0900

    자료구조

---
+++ 2023-10-02/자료구조5.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+
+S = [0]*N
+C = [0]*M
+
+S[0] = A[0]
+answer = 0
+
+for i in range(1,N):
+    S[i] = S[i-1] + A[i]
+
+for i in range(N):
+    temp = S[i] % M
+    if temp ==0:
+        answer += 1
+    C[temp] += 1
+
+for i in range(M):
+    if C[i] > 1:
+        answer += (C[i]*(C[i]-1)//2)
+
+print(answer)
+
